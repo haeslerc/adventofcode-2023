1468f427425a4344a1724661a62c466c9bb686f9
haeslerc <christian.haesler@hotmail.ch>
2023-12-26 15:10:01 +0100

    refactor redundant code

--- src/main/kotlin/day01_1.kt
+++ src/main/kotlin/day01_1.kt
@@ -1,10 +1,8 @@
 fun main() {
     val input = readFile("day01_1")
     var calibrationValue = 0
-    input.forEach {
-        val firstVal = it.first { i -> i in '0'..'9' }
-        val lastVal = it.last { i -> i in '0'..'9' }
-        calibrationValue += ("$firstVal$lastVal").toInt()
-    }
+    input.forEach { s ->
+        val (firstVal, lastVal) = s.filter { it.isDigit() }.run { first() to last() }
+        calibrationValue += ("$firstVal$lastVal").toInt()}
     println(calibrationValue)
 }
